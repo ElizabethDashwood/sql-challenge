83c1b285cbe6ed9c67a812cd6eb3dd814ef3d2d5
Elizabeth Dashwood <dashwoodliz@gmail.com>
2024-04-27 23:17:08 +1000

    Final draft of sql code

--- Data Engineering and Data Analysis for EmployeeSQL.sql
+++ Data Engineering and Data Analysis for EmployeeSQL.sql
@@ -51,10 +51,10 @@ create table dept_manager (
 
 -- Data Analysis
 -- Task 1 List the employee enumber, last name, first name sex and salary of each employee
-select e.emp_no,
-	e.last_name,
-	e.first_name,
-	e.hire_date,
+select e.emp_no AS "employee number",
+	e.last_name AS "last name",
+	e.first_name AS "first name",
+	e.hire_date AS "hire date",
 	e.sex,
 	s.salary
 from employees as e
@@ -63,9 +63,9 @@ s.emp_no = e.emp_no;
 
 -- Task 2 List the first name, last name and hire date for the employees who were hired in 1986
 select 
-first_name,
-last_name,
-hire_date
+first_name AS "first name",
+last_name AS "last name",
+hire_date AS "hire date"
 from employees
 where hire_date like '%/1986';
 
@@ -83,8 +83,9 @@ where hire_date like '%/1986';
 
 
 -- Task 5. List the first name, last name and sex of each employee whose first name is Hercules and whose last name begins with the letter B
-select e.first_name,
-	e.last_name,
+select 
+	e.first_name AS "first name",
+	e.last_name AS "last name",
 	e.sex
 from employees as e
 where first_name = 'Hercules'
@@ -99,7 +100,7 @@ and last_name like 'B%';
 
 -- List the frequency counts, in descending order, of all employee last names (that is, how many employees share each last name)
 
-select e.last_name, count(*) as count
+select e.last_name as "last name", count(*) as count
 from employees as e
 group by e.last_name
 order by count desc;
